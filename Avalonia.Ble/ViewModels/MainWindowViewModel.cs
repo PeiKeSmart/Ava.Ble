f286463d705d4f822b90125bcd25a664567d554a
猿人易 <qcjxberin@outlook.com>
2025-05-13 07:45:19 +0800

    重构项目以支持 Avalonia.Ble 结构
    更新项目文件和解决方案，添加 Avalonia.Ble 项目。
    在 .gitignore 中添加新生成文件夹的忽略规则。
    在 App.axaml 中设置应用程序基本配置和主题样式。
    实现 App.axaml.cs 中的应用程序初始化逻辑。
    添加 avalonia-logo.ico 二进制文件。
    配置 Avalonia.Ble.csproj 的基本属性和依赖项。
    实现 Program.cs 中的应用程序入口点和配置。
    创建 MainWindowViewModel.cs，添加问候字符串属性。
    定义 ViewModelBase.cs 作为视图模型基类。
    在 MainWindow.axaml 中设置主窗口布局和数据绑定。
    实现 MainWindow.axaml.cs 中的构造函数和初始化。
    添加 app.manifest 以支持 Windows 窗口透明和嵌入控件功能。

---
+++ Avalonia.Ble/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace Avalonia.Ble.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase {
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Avalonia.Ble/Views/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Ble.Views;
+public partial class MainWindow : Window {
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
