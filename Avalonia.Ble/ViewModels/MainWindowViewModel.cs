31e0eb021f670e8575ca8d36ff4d29ed8f23a9b6
猿人易 <qcjxberin@outlook.com>
2025-05-13 08:01:48 +0800

    清理重来

--- Avalonia.Ble/ViewModels/MainWindowViewModel.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Avalonia.Ble.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase {
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- Avalonia.Ble/ViewModels/ViewModelBase.cs
+++
@@ -1,5 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Avalonia.Ble.ViewModels;
-public class ViewModelBase : ObservableObject {
-}
